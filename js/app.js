b9b726f78d91b9babdb141cb9e17f4843312cd69
David Gamache <Dave@Davids-MacBook-Pro.local>
2014-04-10 12:44:52 -0500

    woohoo. Pretty good pass at the new structure.

--- js/app.js
+++ js/app.js
@@ -1,121 +1,129 @@
-var dg = {
+(function() {
+  $(function() {
 
-  // Variables
-  $window:                  $(window),
-  $body:                    $('body'),
-  $intro:                   $('.intro'),
-  $name:                    $('.name'),
-  $medium:                  $('.medium'),
-  bodyHeight:               0,
-  currentScrollTop:         0,
-  windowHeight:             0,
-  keyframeHeights:          ['100%', '100%', '50%', '100%'],
-  prevKeyframesHeights:     0,
-  currentKeyframe:          0,
+    // Variables
+    var SCROLL_SPEED_MULTIPLIER =  1, 
+        $window =                  $(window),
+        $body =                    $('body'),
+        $intro =                   $('.intro'),
+        $name =                    $('.name'),
+        $medium =                  $('.medium'),
+        bodyHeight =               0,
+        currentScrollTop =         0,
+        windowHeight =             0,
+        prevKeyframesHeights =     0,
+        currentKeyframe =          0,
+        keyframes = [
+          {
+            'start' : 0,// the keyframe at which you want the animations to start
+            'duration' : '250%',
+            'animations' :  [
+              {
+                'selector'    : '.intro',
+                'translateY'  : -400
+              } , {
+                'selector'    : '.intro-background',
+                'translateY'  : 200
+              } , {
+                'selector'    : '.name',
+                'translateY'  : 0
+              } , {
+                'selector'    : '.medium',
+                'translateY'  : '-100%'
+              }
+            ]
+          } , {
+            'start' : 0,// the keyframe at which you want the animations to start
+            'duration' : '500%',
+            'animations' :  [
+              {
+                'selector'    : '.medium-background',
+                'translateY'  : -50
+              } , {
+                'selector'    : '.other-name',
+                'translateY'  : '-20%'
+              } , {
+                'selector'    : '.phone',
+                'translateX'  : -670
+              } , {
+                'selector'    : '.phone-2',
+                'translateX'  : -750
+              } , {
+                'selector'    : '.phone-3',
+                'translateX'  : -830
+              }
+            ]
+          } , {
+            'start' : 0,// the keyframe at which you want the animations to start
+            'duration' : '100%',
+            'animations' :  []
+          }
+        ]
 
-
-  init: function() {
-    dg.$window.on('scroll', dg.onScroll);
-    dg.buildPage();
-  },
-
-  buildPage: function() {
-    dg.currentScrollTop = dg.$window.scrollTop();
-    dg.windowHeight = dg.$window.height();
-        // Change all of the keyframe heights to px
-    for(i=0;i<dg.keyframeHeights.length;i++) {
-        dg.keyframeHeights[i] = dg.convertPercentToPx(dg.keyframeHeights[i]);
-        dg.bodyHeight += dg.keyframeHeights[i];
-        console.log(dg.keyframeHeights[i]);
+    init = function() {
+      $window.on('scroll', onScroll);
+      buildPage();
     }
-    dg.$intro.height(dg.keyframeHeights[0]);
-    dg.$medium.height(dg.keyframeHeights[1] + dg.keyframeHeights[2]);
-    dg.$body.height(dg.bodyHeight);
-  },
 
-  onScroll: function() {
-
-    dg.currentScrollTop = dg.$window.scrollTop();
-
-    if(dg.currentKeyframe == 0) {
-        dg.scrawler(".intro", '-100%', 0, 0);
-        dg.scrawler(".intro-background", 200, 0, 0);
-        dg.scrawler(".name", '55%', 0, 0);
-        dg.scrawler(".medium", '-100%', 0, 0);
-        // dg.scrawler(".other-name", '20%', 0, 0);
+    buildPage = function() {
+      currentScrollTop = $window.scrollTop();
+      windowHeight = $window.height();
+      for(i=0;i<keyframes.length;i++) {
+          keyframes[i].duration = convertPercentToPx(keyframes[i].duration);
+          bodyHeight += keyframes[i].duration;
+          for(j=0;j<keyframes[i].animations.length;j++) {
+            Object.keys(keyframes[i].animations[j]).forEach(function(key) {
+                keyframes[i].animations[j][key] = convertPercentToPx(keyframes[i].animations[j][key]);
+            });
+          }
+      }
+      $body.height(bodyHeight);
+      $window.scroll(0);
     }
-    if(dg.currentKeyframe == 1) {
-        dg.scrawler(".medium-background", "-5%", 0, 0);
-        dg.scrawler(".other-name", '-20%', 0, 0);
-        dg.scrawler(".phone", 0, -670, 0);
-        dg.scrawler(".phone-2", 0, -750, 0);
-        dg.scrawler(".phone-3", 0, -830, 0);
+
+    onScroll = function() {
+      var animation, transform, translateY;
+      currentScrollTop = $window.scrollTop();
+      for(i=0;i<keyframes[currentKeyframe].animations.length;i++) {
+        animation = keyframes[currentKeyframe].animations[i];
+        if(animation.translateY) {
+          animation.translateY = convertPercentToPx(animation.translateY);
+          translateY = (currentScrollTop - prevKeyframesHeights) * (animation.translateY / keyframes[currentKeyframe].duration);
+        } else {
+          translateY = 0;
+        }
+        if(animation.translateX) {
+          animation.translateX = convertPercentToPx(animation.translateX);
+          translateX = (currentScrollTop - prevKeyframesHeights) * (animation.translateX / keyframes[currentKeyframe].duration);
+        } else {
+          translateX = 0;
+        }
+        $(animation.selector).css({
+          '-webkit-transform': 'translate3d(' + translateX +'px, ' + translateY + 'px, 0)',
+        })
+      }
+      setKeyframe();
     }
-    // if(dg.currentKeyframe == 2) { 
-    //     dg.scrawler(".phone", 0, -500, 0);
-    // }
 
-    dg.setKeyframe();
-
-    // console.log(dg.currentKeyframe + " , " + (dg.keyframeHeights[dg.currentKeyframe] + dg.prevKeyframesHeights));
-    // console.log(dg.currentScrollTop + " vs. " + dg.prevKeyframesHeights);
-    // console.log(dg.$window.scrollTop())
-    // console.log(dg.keyframeHeights[dg.currentKeyframe]);
-    // console.log(dg.currentScrollTop);
-    console.log(dg.currentKeyframe);
-
-  },
-
-  setKeyframe: function() {
-    // console.log(dg.currentKeyframe)
-    if(dg.currentScrollTop > (dg.keyframeHeights[dg.currentKeyframe] + dg.prevKeyframesHeights)) {
-        dg.prevKeyframesHeights += dg.keyframeHeights[dg.currentKeyframe];
-        dg.currentKeyframe++;
-    } else if(dg.currentScrollTop < dg.prevKeyframesHeights && dg.currentScrollTop >= 0) {
-        dg.currentKeyframe--;
-        dg.prevKeyframesHeights -= dg.keyframeHeights[dg.currentKeyframe];
+    setKeyframe = function() {
+      // console.log(currentKeyframe + ': ' + currentScrollTop + ' needs to be greater than ' + (keyframes[currentKeyframe].duration + prevKeyframesHeights) + ' and lower than ' + (bodyHeight - windowHeight) + ' to advance to next keyframe');
+      if(currentScrollTop > (keyframes[currentKeyframe].duration + prevKeyframesHeights) && currentScrollTop <= (bodyHeight - windowHeight)) {
+          prevKeyframesHeights += keyframes[currentKeyframe].duration;
+          currentKeyframe++;
+      } else if(currentScrollTop < prevKeyframesHeights && currentScrollTop >= 0) {
+          currentKeyframe--;
+          prevKeyframesHeights -= keyframes[currentKeyframe].duration;
+      }
     }
-  },
 
-  convertPercentToPx: function(value) {
-    if(typeof value === "string" && value.match(/%/g)) {
-        value = (parseFloat(value) / 100) * dg.windowHeight;
+    convertPercentToPx = function(value) {
+      if(typeof value === "string" && value.match(/%/g)) {
+          value = (parseFloat(value) / 100) * windowHeight;
+      }
+      return value;
     }
-    return value;
-  },
 
-  scrawler: function(selector, moveY, moveX, opacity) {
-    moveY = dg.convertPercentToPx(moveY);
-    moveX = dg.convertPercentToPx(moveX);
-    if(moveY && moveX) {
-        var scrollRateY = moveY / dg.keyframeHeights[dg.currentKeyframe];
-        var scrollRateX = moveX / dg.keyframeHeights[dg.currentKeyframe];
-        var distanceToMoveY = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRateY
-        var distanceToMoveX = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRateX
-        $(selector).css({
-          '-webkit-transform': 'translate3d('+ distanceToMoveX + 'px, ' + distanceToMoveY + 'px, 0)',
-        });
-    } else if(moveY) {
-        var scrollRate = moveY / dg.keyframeHeights[dg.currentKeyframe];
-        var distanceToMove = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRate
-        $(selector).css({
-          '-webkit-transform': 'translate3d(0,' + distanceToMove + 'px, 0)',
-        });
-    } else if(moveX) {
-        var scrollRate = moveX / dg.keyframeHeights[dg.currentKeyframe];
-        var distanceToMove = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRate
-        $(selector).css({
-          '-webkit-transform': 'translate3d(' + distanceToMove + 'px, 0, 0)',
-        });
-    }
-    if(opacity) {
-      //dummy fade to 0 right now
+    init();
 
-    }
-  }
-
-}
-
-$(function() {
-  dg.init();
-})
+  })
+}).call(this);
