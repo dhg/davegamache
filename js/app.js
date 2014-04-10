21e6de6c4ede4d80f2cd30710106ea4fff4a7c10
David Gamache <Dave@Davids-MacBook-Pro.local>
2014-04-10 09:35:29 -0500

    adding phones and some other code for testing

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var dg = {
   bodyHeight:               0,
   currentScrollTop:         0,
   windowHeight:             0,
-  keyframeHeights:          ['100%', '100%', 800],
+  keyframeHeights:          ['100%', '100%', '50%', '100%'],
   prevKeyframesHeights:     0,
   currentKeyframe:          0,
 
@@ -24,8 +24,9 @@ var dg = {
     dg.windowHeight = dg.$window.height();
         // Change all of the keyframe heights to px
     for(i=0;i<dg.keyframeHeights.length;i++) {
-        dg.keyframeHeights[i] = dg.convertPercentToDecimal(dg.keyframeHeights[i]);
+        dg.keyframeHeights[i] = dg.convertPercentToPx(dg.keyframeHeights[i]);
         dg.bodyHeight += dg.keyframeHeights[i];
+        console.log(dg.keyframeHeights[i]);
     }
     dg.$intro.height(dg.keyframeHeights[0]);
     dg.$medium.height(dg.keyframeHeights[1] + dg.keyframeHeights[2]);
@@ -37,19 +38,22 @@ var dg = {
     dg.currentScrollTop = dg.$window.scrollTop();
 
     if(dg.currentKeyframe == 0) {
+        dg.scrawler(".intro", '-100%', 0, 0);
         dg.scrawler(".intro-background", 200, 0, 0);
-        dg.scrawler(".name", '65%', 0, 0);
-        dg.scrawler(".intro", '-101%', 0, 0);
-        dg.scrawler(".medium", '-101%', 0, 0);
-        dg.scrawler(".test-item-1", '-10%', 400, 0);
-        dg.scrawler(".test-item-2", 45, -20, 0);
-        dg.scrawler(".test-item-3", '37%', 700, 0);
-        dg.scrawler(".test-item-4", '-11%', 200, 0);
-        dg.scrawler(".test-item-5", 900, 40, 0);
-        dg.scrawler(".test-item-6", '-101%', 393, 0);
-        dg.scrawler(".test-item-7", '50%', 200, 0);
+        dg.scrawler(".name", '55%', 0, 0);
+        dg.scrawler(".medium", '-100%', 0, 0);
+        // dg.scrawler(".other-name", '20%', 0, 0);
     }
-
+    if(dg.currentKeyframe == 1) {
+        dg.scrawler(".medium-background", "-5%", 0, 0);
+        dg.scrawler(".other-name", '-20%', 0, 0);
+        dg.scrawler(".phone", 0, -670, 0);
+        dg.scrawler(".phone-2", 0, -750, 0);
+        dg.scrawler(".phone-3", 0, -830, 0);
+    }
+    // if(dg.currentKeyframe == 2) { 
+    //     dg.scrawler(".phone", 0, -500, 0);
+    // }
 
     dg.setKeyframe();
 
@@ -57,11 +61,13 @@ var dg = {
     // console.log(dg.currentScrollTop + " vs. " + dg.prevKeyframesHeights);
     // console.log(dg.$window.scrollTop())
     // console.log(dg.keyframeHeights[dg.currentKeyframe]);
-    // console.log(dg.currentKeyframe);
+    // console.log(dg.currentScrollTop);
+    console.log(dg.currentKeyframe);
 
   },
 
   setKeyframe: function() {
+    // console.log(dg.currentKeyframe)
     if(dg.currentScrollTop > (dg.keyframeHeights[dg.currentKeyframe] + dg.prevKeyframesHeights)) {
         dg.prevKeyframesHeights += dg.keyframeHeights[dg.currentKeyframe];
         dg.currentKeyframe++;
@@ -71,7 +77,7 @@ var dg = {
     }
   },
 
-  convertPercentToDecimal: function(value) {
+  convertPercentToPx: function(value) {
     if(typeof value === "string" && value.match(/%/g)) {
         value = (parseFloat(value) / 100) * dg.windowHeight;
     }
@@ -79,21 +85,32 @@ var dg = {
   },
 
   scrawler: function(selector, moveY, moveX, opacity) {
-    moveY = dg.convertPercentToDecimal(moveY);
-    moveX = dg.convertPercentToDecimal(moveX);
-    if(moveY) {
+    moveY = dg.convertPercentToPx(moveY);
+    moveX = dg.convertPercentToPx(moveX);
+    if(moveY && moveX) {
+        var scrollRateY = moveY / dg.keyframeHeights[dg.currentKeyframe];
+        var scrollRateX = moveX / dg.keyframeHeights[dg.currentKeyframe];
+        var distanceToMoveY = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRateY
+        var distanceToMoveX = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRateX
+        $(selector).css({
+          '-webkit-transform': 'translate3d('+ distanceToMoveX + 'px, ' + distanceToMoveY + 'px, 0)',
+        });
+    } else if(moveY) {
         var scrollRate = moveY / dg.keyframeHeights[dg.currentKeyframe];
         var distanceToMove = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRate
         $(selector).css({
           '-webkit-transform': 'translate3d(0,' + distanceToMove + 'px, 0)',
         });
-    }
-    if(moveX) {
+    } else if(moveX) {
         var scrollRate = moveX / dg.keyframeHeights[dg.currentKeyframe];
         var distanceToMove = (dg.currentScrollTop - dg.prevKeyframesHeights) * scrollRate
         $(selector).css({
           '-webkit-transform': 'translate3d(' + distanceToMove + 'px, 0, 0)',
         });
+    }
+    if(opacity) {
+      //dummy fade to 0 right now
+
     }
   }
 
