1bcf8010f6baefb7c54e26f28cc674b679ed0e75
Dave Gamache <dhgamache@gmail.com>
2017-03-09 12:42:33 -0800

    adding evie test site to send aroudn

---
+++ evie-new-site/static/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+	particlesJS.load('particleNetwork', 'static/js/particle-config.json', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+
+});
